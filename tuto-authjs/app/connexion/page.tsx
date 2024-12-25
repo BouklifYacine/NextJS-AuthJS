cbc9bbe1f74cea0eaec9dd048c1851f79575e275
YacineBoukliff <yacine69800@live.fr>
2024-12-25 10:25:48 +0100

    Tutofini

--- tuto-authjs/app/connexion/page.tsx
+++ tuto-authjs/app/connexion/page.tsx
@@ -1,20 +1,20 @@
-"use client"
-import { useSession } from 'next-auth/react'
+import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
-const PageConnexion = () => {
-    const { data: session } = useSession({
-        required: true,
-        onUnauthenticated() {
-            redirect('/')
-        },
-    })
-
+const PageConnexion = async () => {
+    const session = await auth()
+    console.log("Session dans PageConnexion:", session)
+    
+    if (!session) {
+        console.log("Pas de session, redirection...")
+        redirect('/')
+    }
+    
     return (
         <div>
             <h1>Je suis connecté</h1>
-            <p>Email: {session?.user?.email}</p>
-            <p> Nom : {session?.user?.name} </p>
+            <p>Email: {session.user?.email}</p>
+            <p> Nom : {session.user?.name}</p>
         </div>
     )
 }
